6f077e7e34fb96483034c9bf18afea02920e9f39
regorie <81633444+regorie@users.noreply.github.com>
2025-07-22 14:02:00 +0900

    Update build_all.sh

--- build_all.sh
+++ build_all.sh
@@ -1,4 +1,4 @@
-gcc ./src/CBOW.c -o cbow
-gcc ./src/CBOW_NS.c -o cbowns
-gcc ./src/Skipgram.c -o skipgram
-gcc ./src/Skipgram_NS.c -o skipgramns
+gcc ./src/CBOW.c -o cbow -lm -pthread -O3 -march=native -funroll-loops
+gcc ./src/CBOW_NS.c -o cbowns -lm -pthread -O3 -march=native -funroll-loops
+gcc ./src/Skipgram.c -o skipgram -lm -pthread -O3 -march=native -funroll-loops
+gcc ./src/Skipgram_NS.c -o skipgramns -lm -pthread -O3 -march=native -funroll-loops
